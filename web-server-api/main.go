89b76c86f9a09933f2fa63a15be825d81652d67a
404akshat <akshat.insharma@gmail.com>
2026-05-11 18:14:27 +0530

    Initialised GET /items

--- web-server-api/main.go
+++ web-server-api/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type to_do_list struct {
 	ID       string `json:"id"`
 	Title    string `json:"title"`
@@ -11,4 +17,14 @@ var tdlists = []to_do_list{
 	{ID: "1", Title: "Apply for Internships", Time: "10 AM", Priority: 1},
 	{ID: "2", Title: "Big Chungus Cluster", Time: "2 PM", Priority: 1},
 	{ID: "3", Title: "Coursera", Time: "6 PM", Priority: 2},
+}
+
+func getList(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, tdlists)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/items", getList)
+	router.Run("localhost:8080")
 }
